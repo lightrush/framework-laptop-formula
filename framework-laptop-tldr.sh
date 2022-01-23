03dca4566fd8b9a27f3ebbfa6d14682dc865fb74
Nicolay Doytchev <self@ndoytchev.com>
2022-01-23 10:40:46 -0500

    Update apt before trying to install salt-minion
    - Resolves a corner case when tldr script is run on a 
    fresh jammy install before anyone tried to update packages
    and tries to install upstream salt instead.

--- framework-laptop-tldr.sh
+++ framework-laptop-tldr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if ! sudo apt-get -y install salt-minion ; then wget -O /tmp/bootstrap-salt.sh https://bootstrap.saltproject.io && sudo sh /tmp/bootstrap-salt.sh ; fi \
+if ! sudo apt-get -y update && sudo apt-get -y install salt-minion ; then wget -O /tmp/bootstrap-salt.sh https://bootstrap.saltproject.io && sudo sh /tmp/bootstrap-salt.sh ; fi \
   && wget -O framework-laptop-formula-main.zip https://github.com/lightrush/framework-laptop-formula/archive/refs/heads/main.zip && unzip -o framework-laptop-formula-main.zip \
   && sudo salt-call -l error --local --file-root="$(pwd)/framework-laptop-formula-main" state.apply framework-laptop
