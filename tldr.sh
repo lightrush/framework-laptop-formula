0f72453c110ed8078a3f8e55097c5e6b7818ff03
Nicolay Doytchev <self@ndoytchev.com>
2022-01-22 22:42:32 -0500

    Add a tldr.sh script to replace TL;DR copy-pasta.

---
+++ tldr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if lspci -n | grep -q '8086:2725' && echo $(uname -r) | grep -q 5.11 ; then sudo rm -f /lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm ; sudo rmmod iwlmvm ; sudo rmmod iwlwifi ; sudo modprobe iwlwifi && /bin/bash -c 'while ! nslookup google.com 8.8.8.8 &> /dev/null ; do echo No internet connection. Waiting... ; sleep 10 ; done' ; fi \
+  && if ! which salt-call 1>/dev/null; then if apt-cache policy salt-minion | grep -qi candidate ; then sudo apt-get -y install salt-minion ; else wget -O /tmp/bootstrap-salt.sh https://bootstrap.saltproject.io && sudo sh /tmp/bootstrap-salt.sh ; fi ; fi \
+  && wget -O framework-laptop-formula-main.zip https://github.com/lightrush/framework-laptop-formula/archive/refs/heads/main.zip && unzip -o framework-laptop-formula-main.zip \
+  && sudo salt-call -l error --local --file-root="$(pwd)/framework-laptop-formula-main" state.apply framework-laptop
